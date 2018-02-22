56e5e6e8e8f642d4209f2ff8995647fc05085631
Rita F <32108877+jamhouston@users.noreply.github.com>
2018-02-22 18:42:23 +0300

    Update rational.h

--- filatova_m_a/rational/rational.h
+++ filatova_m_a/rational/rational.h
@@ -10,46 +10,54 @@ struct Rational
 	Rational() {}
 	explicit Rational(const int numenator, const int denominator);
 	Rational(const int numenator);
-	bool operator==(const Rational& lhs);
-	bool operator!=(const Rational& lhs);
-	bool operator>(const Rational& a) const;
-	bool operator<(const Rational& a) const;
-	bool operator>=(const Rational& a) const;
-    bool operator<=(const Rational& a) const;
-	Rational& operator+(const Rational& rhs, const Rational& lhs);
-	Rational& operator+(const Rational& rhs, const int lhs);
-	Rational& operator+(const int rhs, const Rational& lhs);
-	Rational& operator-(const Rational& rhs, const Rational& lhs);
-	Rational& operator-(const Rational& rhs, const int lhs);
-	Rational& operator-(const int rhs, const Rational& lhs);
+	bool operator==(const Rational& lhs) const;
+	bool operator!=(const Rational& lhs) const;
+	bool operator>(const Rational& lhs) const;
+	bool operator<(const Rational& lhs) const;
+	bool operator>=(const Rational& lhs) const;
+	bool operator<=(const Rational& lhs) const;
 	Rational& operator+=(const Rational& lhs);
 	Rational& operator+=(const int lhs);
 	Rational& operator-=(const Rational& lhs);
 	Rational& operator-=(const int lhs);
-	Rational& operator*(const Rational& rhs, const Rational& lhs);
-	Rational& operator*(const Rational& rhs, const int lhs);
-	Rational& operator*(const int rhs, const Rational& lhs);
-	Rational& operator/(const Rational& rhs, const Rational& lhs);
-	Rational& operator/(const Rational& rhs, const int lhs);
-	Rational& operator/(const int rhs, const Rational& lhs);
 	Rational& operator*=(const Rational& lhs);
 	Rational& operator*=(const int lhs);
 	Rational& operator/=(const Rational& lhs);
 	Rational& operator/=(const int lhs);
+
+
 	std::ostream &writeTo(std::ostream &ostr) const;
 	std::istream &readFrom(std::istream &istrm);
 
 
-	int num { 0 };
-	int den { 1 };
-}
+	long int num{ 0 };
+	long int den{ 1 };
+	static const char leftBrace{ '{' };
+	static const char separator{ '/' };
+	static const char rightBrace{ '}' };
+};
 
-std::ostream& operator<<(std::ostream& ostrm, Complex& rhs);
-std::istream& operator>>(std::istream& istrm, Complex& rhs);
 
-private int GCD(int lhs,  int rhs);
-private int LCM(int lhs, int rhs);
-private void Reduce(ational& ratio);
+Rational& operator+(const Rational& rhs, const Rational& lhs);
+Rational& operator+(const Rational& rhs, const int lhs);
+Rational& operator+(const int rhs, const Rational& lhs);
+Rational& operator-(const Rational& rhs, const Rational& lhs);
+Rational& operator-(const Rational& rhs, const int lhs);
+Rational& operator-(const int rhs, const Rational& lhs);
+
+Rational& operator*(const Rational& rhs, const Rational& lhs);
+Rational& operator*(const Rational& rhs, const int lhs);
+Rational& operator*(const int rhs, const Rational& lhs);
+Rational& operator/(const Rational& rhs, const Rational& lhs);
+Rational& operator/(const Rational& rhs, const int lhs);
+Rational& operator/(const int rhs, const Rational& lhs);
+
+std::ostream& operator<<(std::ostream& ostrm, Rational& rhs);
+std::istream& operator>>(std::istream& istrm, Rational& rhs);
+
+int GCD(int lhs, int rhs);
+int LCM(int lhs, int rhs);
+Rational Reduce(Rational& ratio);
 bool testParse(const std::string& str);
 
 
