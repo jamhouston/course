3614996bb495106fe7292615534b8eda3c7ef688
Rita F <32108877+jamhouston@users.noreply.github.com>
2018-02-22 21:55:51 +0300

    Update array.h

--- filatova_m_a/array/array.h
+++ filatova_m_a/array/array.h
@@ -12,15 +12,17 @@ public:
 	Array() = default;
 	~Array() = default;
 	Array(const int item);
-	Array& operator=(const Array& rhs) = default;
-	int& operator[](const ptrdiff_t item);
+	Array& operator=(const Array& rhs);
+	const int& Array::operator[](const ptrdiff_t index) const;
+	void Push_back(const int item);
+	void Insert(const int pos, const int item);	void Add();
+	int Size() const;
 
 private:
-    ptrdiff_t capacity_ { 0 };
-    ptrdiff_t size_ { 0 };
-    int* pdata_ { nullptr };
-    void add();
-    int size() const;
+	ptrdiff_t capacity_{ 0 };
+	ptrdiff_t size_{ 0 };
+	int* pdata_{ nullptr };
+
 
 
 };
