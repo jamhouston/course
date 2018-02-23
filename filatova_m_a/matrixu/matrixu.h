a452736ea20d3bc83d08f32e0f904399cd9cefde
Rita F <32108877+jamhouston@users.noreply.github.com>
2018-02-23 12:49:33 +0300

    Update matrixu.h

--- filatova_m_a/matrixu/matrixu.h
+++ filatova_m_a/matrixu/matrixu.h
@@ -1,25 +1,33 @@
-#ifndef MatrixU_H
-#define MATRIXU_H 
+#ifndef MATRIX_U
+#define  MATRIX_U
 
 #include <iosfwd>
+#include <iostream>
+#include <string>
 
 class MatrixU
 {
 public:
-	MatrixU() = default;
+	explicit MatrixU() = default;
 	~MatrixU();
 	MatrixU(const ptrdiff_t nRow, const ptrdiff_t nCol);
-	int& At(ptrdiff_t iRow, ptrdiff_t iCol);
-	const int& At(ptrdiff_t iRow, ptrdiff_t iCol) const;
-	void Resize(Matrix& mat, const ptrdiff_t nRow, ptrdiff_t nCol);
+	ptrdiff_t getRowCount();
+	ptrdiff_t getColCount();
+
+	ptrdiff_t At(ptrdiff_t iCol, ptrdiff_t iRow);
+	void Set(ptrdiff_t iRow, ptrdiff_t iCol, ptrdiff_t item);
+	void Set(ptrdiff_t item);
+	void SumMat(MatrixU& mat);
+	void MultInt(ptrdiff_t value);
+	void MultMat(MatrixU& mat);
+	void SubMat(MatrixU& mat);
+
 
 private:
-	ptrdiff_t nRow_ { 0 };
-	ptrdiff_t nCol_ { 0 };
-	*pdata_ = { nullptr };
-
-
-}
+	int* pData_{ nullptr };
+	ptrdiff_t nRow_{ 0 };
+	ptrdiff_t nCol_{ 0 };
+};
 
 
 #endif
